9d7e424239c8845067a9c9492e02b83e7ddbe315
jungod <zhfk6095@naver.com>
2025-07-09 08:17:49 +0900

    updated code

--- p2ch10/dsets.py
+++ p2ch10/dsets.py
@@ -58,4 +58,168 @@ def getCandidateInfoList(requireOnDisk_bool=True):
                 continue
 
             isNodule_bool = bool(int(row[4]))
-            
+            candidateCenter_xyz = tuple([float(x) for x in row[1:4]])
+
+            candidateDiameter_mm = 0.0
+            for annotation_tup in diameter_dict.get(series_uid, []):
+                annotationCenter_xyz, annotationDiameter_mm = annotation_tup
+                for i in range(3):
+                    delta_mm = abs(candidateCenter_xyz[i] - annotationCenter_xyz[i])
+                    if delta_mm > annotationDiameter_mm / 4:
+                        break
+                else:
+                    candidateDiameter_mm = annotationDiameter_mm
+                    break
+
+# 튜플안에 있는 내용을 순차적으로 리스트에 넣어서 저장함.
+            candidateInfo_list.append(CandidateInfoTuple(
+                isNodule_bool,
+                candidateDiameter_mm,
+                series_uid,
+                candidateCenter_xyz,
+            ))
+
+# 모든 결절을 내림차순으로 정렬하고, 그 뒤에는 결절이 아닌 샘플이 이어진 데이터가 된다.
+    candidateInfo_list.sort(reverse=True)
+    return candidateInfo_list
+
+# 이제 디스크에서 CT 데이터를 가져와 파이산 객체로 변환해서 3차원 결절 밀도 데이터로 사용하게 하는 작업!
+class Ct:
+    def __init__(self, series_uid):
+# 파일 이름을 format을 활용해서 series_uid.mhd로 만든다. [0]은 찾은 파일 리스트 중 첫 파일만 사용한다는 의미!
+        mhd_path = glob.glob(
+            'data-unversioned/part2/lung/subset*/{}.mhd'.format(series_uid)
+        )[0]
+        
+        
+        ct_mhd = sitk.ReadImage(mhd_path) #CT 이미지 전체를 메모리에 불러오는 작업.
+# sitk.GetArrayFromImage로 ct_mhd를 numpy 배열로 변환함. 이 배열은 3d ct의 실제 픽셀 (hu)를 가짐.
+        ct_a = np.array(sitk.GetArrayFromImage(ct_mhd), dtype=np.float32) 
+# 공기는 -1000HU, 물은 0HU, 뼈는 +1000HU임.        
+        ct_a.clip(-1000, 1000, ct_a)
+        
+        self.series_uid = series_uid
+        self.hu_a = ct_a
+
+# 밀리미터로 나타낸 위치정보는 배열 인덱스로 넣어서는 제대로 작업이 안됨. x,y,z -> i,r,c 복셀 주소 기반 좌표계로 변환함.
+        self.origin_xyz = XyzTuple(*ct_mhd.GetOrigin()) # ct원점의 실제 위치
+        self.vxSize_xyz = XyzTuple(*ct_mhd.GetSpacing()) # 복셀 하나의 크기
+        self.direction_a = np.array(ct_mhd.GetDirection()).reshape(3, 3) # 각 축이 어떤 방향을 바라보는지
+
+# xyz -> irc 표
+    def getRawCandidate(self, center_xyz, width_irc):
+        center_irc = xyz2irc(
+            center_xyz,
+            self.origin_xyz, # 시작점
+            self.vxSize_xyz, # 복셀크기
+            self.direction_a # 방향 정보
+        )
+# ct는 일반적으로 512 x 512이고, 인덱스 차원은 100~250개의 단면으로 이루어짐.
+        slice_list = []
+# center_irc 는 (i,r,c) = (z,y,x) 형태. axis = 0, 1, 2(z,y,x 축), center_val은 각 축의 중심 인덱스 값.
+        for axis, center_val in enumerate(center_irc):
+# 자르려는 영역의 시작점/끝점 인덱스 계산, width_irc[axis]: 잘라낼 범위 크기,  center_val - width/2 : 중심 기준으로 좌우 자르기.             
+            start_ndx = int(round(center_val - width_irc[axis]/2))
+            end_ndx = int(start_ndx + width_irc[axis])
+# 중심 좌표가 ct내부에 있는지 확인. self.hu_a.shape는 (z,y,x) 축의 전체 크기.
+            assert center_val >= 0 and center_val < self.hu_a.shape[axis], repr([self.series_uid, center_xyz, self.origin_xyz, self.vxSize_xyz, center_irc, axis])
+
+            if start_ndx < 0: # 시작 인덱스가 0일 경우에는 0부터 시작해요
+                start_ndx = 0 
+                end_ndx = int(width_irc[axis]) 
+
+            if end_ndx > self.hu_a.shape[axis]: # 끝 인덱스가 ct의 크기를 넘을 경우
+                end_ndx = self.hu_a.shape[axis] # ct크기 한계로 자른다.
+                start_ndx = int(self.hu_a.shape[axis] - width_irc[axis])
+
+            slice_list.append(slice(start_ndx, end_ndx)) # 슬라이싱 범위 저장. 총 end_ndx - start_ndx 개수 만큼.
+# self.hu_a는 ct전체 3D배열 (shape=(z,y,x)). slice
+        ct_chunk = self.hu_a[tuple(slice_list)] # 패치 자르기
+# chunk는 슬라이스 z축만큼 있는 3D 부피덩어리
+        return ct_chunk, center_irc
+    
+
+# lru_cache는 최근 사용한 결과를 캐시에 저장해서 사용하는 데코레이터
+@functools.lru_cache(1, typed=True)
+def getCt(series_uid):
+    return Ct(series_uid)
+
+
+# 캐싱 데코레이터
+@raw_cache.memorize(typed=True)
+def getCtRawCandidate(series_uid, center_xyz, width_irc):
+    ct = getCt(series_uid)
+    ct_chunk, center_irc = ct.getRawCandidate(center_xyz, width_irc)
+    return ct_chunk, center_irc
+
+
+class LunaDataset(Dataset):
+    def __init__(self,
+                 val_stride=0, # val_set 분리 간격
+                 isValSet_bool=None, # 이 데이터셋이 검증용인지 아닌지 나타냄
+                 series_uid=None, # 특정 CT 시리즈 id하나만 필터링해서 사용할때 씀.
+            ):
+# 모든 CT의 결절 후보 정보 리스트를 반환하는 함수이다.
+        self.candidateInfo_list = copy.copy(getCandidateInfoList())
+
+# series_uid가 주어졌다면, 리스트에서 해당 시리즈의 결절 후보들만 남긴다.
+        if series_uid:
+            self.candidateInfo_list = [
+                x for x in self.candidateInfo_list if x.series_uid == series_uid
+            ]
+
+# 검증용 데이터셋 만들기.
+        if isValSet_bool:
+            assert val_stride > 0, val_stride
+            self.candidateInfo_list = self.candidateInfo_list[::val_stride]
+            assert self.candidateInfo_list # 결과 리스트가 비어있으면 안됨.
+        elif val_stride > 0:
+            del self.candidateInfo_list[::val_stride] # 전체 후보리스트에서 val_stride 간격으로 삭제한다
+            assert self.candidateInfo_list
+
+        log.info("{!r}: {} {} samples".format(
+            self, # 현재 데이터셋 객체. !r은 self객체의 문자열 표현.
+            len(self.candidateInfo_list), # 후보 수
+            "validation" if isValSet_bool else "training", # 어떤 용도인지
+        ))
+
+    def __len__(self):
+        return len(self.candidateInfo_list) # 결절 후보리스트의 길이를 반환.
+
+    def __getitem__(self, ndx): # ndx는 정수 인덱스
+
+        '''CandidateInfoTuple(
+        isNodule_bool=True/False,
+        diameter_mm=3.5,
+        series_uid="1.2.3...",
+        center_xyz=(x, y, z)'''
+
+        candidateInfo_tup = self.candidateInfo_list[ndx] 
+        width_irc = (32, 48, 48) # 슬라이스 32개, 높이 49, 너비 48로 자름.
+
+# getCtRawCandidate는 Ct를 불러오고 center_xyz는 위치를 기준으로, width_irc크기만큼 자른 3d패치(candidate_a)를 리턴한다.
+        candidate_a, center_irc = getCtRawCandidate(
+            candidateInfo_tup.series_uid,
+            candidateInfo_tup.center_xyz,
+            width_irc,
+        )
+
+# candidate_a 는 넘파이 배열이므로 텐서로 바꿔줌.
+        candidate_t = torch.from_numpy(candidate_a)
+        candidate_t = candidate_t.to(torch.float32) # 타입을 float32로 바꿔줌
+        candidate_t = candidate_t.unsqueeze(0) # 텐서 앞에 채널 차원을 추가 [32, 48, 48] -> [1, 32, 48, 48]
+
+        pos_t = torch.tensor([ # 정답 레이블 생성
+            not candidateInfo_tup.isNodule_bool, # 결절이면 [0,1] 아니먄 [1,0]
+            candidateInfo_tup.isNodule_bool
+        ],
+        dtype=torch.long, # 정수향 클래스 레이블 사용.
+        )
+
+# 튜플로 반환.
+        return (
+            candidate_t, # 입력 CT 패치 (Tensor)
+            pos_t, # 라벨 (이진)
+            candidateInfo_tup.series_uid, # 시리즈 ID (문자열)
+            torch.tensor(center_irc), # 중심 위치 (IRC 좌표)
+        )
--- p2ch11/model.py
+++ p2ch11/model.py
@@ -2,82 +2,89 @@ import math
 
 from torch import nn as nn
 
-from util.logconf import logging
-
-log = logging.getLogger(__name__)
-# log.setLevel(logging.WARN)
-# log.setLevel(logging.INFO)
-log.setLevel(logging.DEBUG)
-
 
 class LunaModel(nn.Module):
-    def __init__(self, in_channels=1, conv_channels=8):
-        super().__init__()
+    def __init__(self, in_channels=1, conv_channels=8): # 입력 채널 1, 첫 conv layer의 출력 채널 수 8
+        super().__init__() # 부모클래스인 nn.Module의 초기화 함수
 
-        self.tail_batchnorm = nn.BatchNorm3d(1)
+# 테일 : 입력값을 이동시키고 비율을 조정해서 정규화함.
+        self.tail_batchnorm = nn.BatchNorm3d(1) # (B, 1, D, H, W)
 
-        self.block1 = LunaBlock(in_channels, conv_channels)
-        self.block2 = LunaBlock(conv_channels, conv_channels * 2)
-        self.block3 = LunaBlock(conv_channels * 2, conv_channels * 4)
-        self.block4 = LunaBlock(conv_channels * 4, conv_channels * 8)
+#  백본: 특징 추출
+        self.block1 = LunaBlock(in_channels, conv_channels) # 입력 채널 1 -> 8
+        self.block2 = LunaBlock(conv_channels, conv_channels * 2) # 채널 8 -> 16
+        self.block3 = LunaBlock(conv_channels * 2, conv_channels * 4) # 채널 16 -> 32
+        self.block4 = LunaBlock(conv_channels * 4, conv_channels * 8) # 채널 32 -> 64
 
-        self.head_linear = nn.Linear(1152, 2)
-        self.head_softmax = nn.Softmax(dim=1)
+# 헤드 : 분류.  1152는 flatten된 feature vector의 길이.
+        self.head_linear = nn.Linear(1152, 2) # block4의 출력 텐서를 벡터로 펼쳐서 2개의 클래스(결절/비결절) 분류
+        self.head_softmax = nn.Softmax(dim=1) # 각 행마다 softmax함.
 
-        self._init_weights()
+        self._init_weights() # 가중치 초기화
 
-    # see also https://github.com/pytorch/pytorch/issues/18182
-    def _init_weights(self):
-        for m in self.modules():
-            if type(m) in {
+    def _init_weights(self): # 모델 내부 레이어들을 순회하며 가중치와 bias를 직접 초기화.
+        for m in self.modules(): # 모델 내 모든 하위 레이어를 하나씩 반환함.
+            if type(m) in { # 현재 레이어 타입이 아래 5자지중 하나라면 초기화한다.
                 nn.Linear,
                 nn.Conv3d,
                 nn.Conv2d,
                 nn.ConvTranspose2d,
                 nn.ConvTranspose3d,
             }:
-                nn.init.kaiming_normal_(
+                nn.init.kaiming_normal_( # kaiming normal 방식으로 초기화
                     m.weight.data, a=0, mode='fan_out', nonlinearity='relu',
                 )
-                if m.bias is not None:
+                if m.bias is not None: # bias가 존재할 경우 초기화
+# fan_out : 출력 채널 수 x 커널 사이즈 -> 출력 방향으로 몇 개의 뉴런에 연결되는지
+# bound : 정규분포 범위 설정용 -> bias 초기화 범위 결정
                     fan_in, fan_out = \
                         nn.init._calculate_fan_in_and_fan_out(m.weight.data)
                     bound = 1 / math.sqrt(fan_out)
-                    nn.init.normal_(m.bias, -bound, bound)
+                    nn.init.normal_(m.bias, -bound, bound) # bias를 정규분포로 초기화.
 
 
+    def forward(self, input_batch): # input_batch : [batch_size, 1, D, H, W] 크기의 3D CT 입력
+# forward는 model(input_batch) 호출 시 자동 실행되는 함수.
+        
+        bn_output = self.tail_batchnorm(input_batch) # 입력을 정규화함.
 
-    def forward(self, input_batch):
-        bn_output = self.tail_batchnorm(input_batch)
-
+# 각 블록을 통과하면서 고차원 피처 추출
         block_out = self.block1(bn_output)
         block_out = self.block2(block_out)
         block_out = self.block3(block_out)
         block_out = self.block4(block_out)
 
+# Flatten 펼치기
+# view(..., -1)은 flatten 연산임. batch_size는 그대로 두고 나머지를 모두 1차원으로 펼침.
+# 결과는 [batch_size, feature_dim] 형태의 텐서로 변환됨.
         conv_flat = block_out.view(
             block_out.size(0),
             -1,
         )
-        linear_output = self.head_linear(conv_flat)
+        linear_output = self.head_linear(conv_flat) # 특징 벡터를 받아서 2개의 클래스로 분류함.
 
-        return linear_output, self.head_softmax(linear_output)
+# 둘 다 리턴해서 필요에 따라 선택적으로 사용 가능함.
+        return linear_output, self.head_softmax(linear_output) # raw logits(모델의 출력점수), softmax확률(총합 1, 각 클래스 확률)
 
 
 class LunaBlock(nn.Module):
     def __init__(self, in_channels, conv_channels):
         super().__init__()
 
+# 입력 채널 수 -> 출력 채널 수로 3D 합성곱
+# 커널 : 3x3x3 필터, padding=1: 출력크기를 입력과 동일하게 유지, bias=True:bias파라미터 포함.
+
         self.conv1 = nn.Conv3d(
-            in_channels, conv_channels, kernel_size=3, padding=1, bias=True,
+            in_channels, conv_channels, kernel_size=3, padding=1, bias=True
         )
-        self.relu1 = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv3d(
-            conv_channels, conv_channels, kernel_size=3, padding=1, bias=True,
+        self.relu1 = nn.ReLU(inplace=True) # ReLU 활성화 함수 적용, inplace=True:메모리 사용을 줄이기 위해 입력을 바로 수정.
+        self.conv2 = nn.Conv3d( # 합성곱 한번 더 수행함.
+            conv_channels, conv_channels, kernel_size=3, padding=1, bias=True # 이번에는 입력 채널, 출력 채널 같음.
         )
         self.relu2 = nn.ReLU(inplace=True)
 
-        self.maxpool = nn.MaxPool3d(2, 2)
+# 입력의 크기를 절반으로 줄임. kernel_size=2, stride=2. 2x2x2 공간에서 가장 큰 값만 남김.
+        self.maxpool = nn.MaxPool3d(2, 2) 
 
     def forward(self, input_batch):
         block_out = self.conv1(input_batch)
